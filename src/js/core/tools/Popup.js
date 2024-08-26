c1a2bd88de2d6ffc6fc30a80d6469f51322aa7ed
Kirill Korostelev <kirill.97nsk@gmail.com>
2024-08-26 16:42:55 +0700

    fixed incorrect behavior when opening context menu at the bottom of the table

--- src/js/core/tools/Popup.js
+++ src/js/core/tools/Popup.js
@@ -196,8 +196,7 @@ export default class Popup extends CoreFeature{
 		}
 		
 		//move menu to start on bottom edge if it is too close to the edge of the screen
-		let offsetHeight = Math.max(this.container.offsetHeight, scrollTop ? this.container.scrollHeight : 0);
-		if((y + this.element.offsetHeight) > offsetHeight) {
+		if((y + this.element.offsetHeight) > Math.max(this.container.offsetHeight, scrollTop ? this.container.scrollHeight : 0)) {
 			if(parentEl){
 				switch(position){
 					case "bottom":
@@ -209,7 +208,7 @@ export default class Popup extends CoreFeature{
 				}
 				
 			}else{
-				this.element.style.height = offsetHeight + "px";
+				this.element.style.top = (parseInt(this.element.style.top) - this.element.offsetHeight) + "px";
 			}
 		}
 	}
